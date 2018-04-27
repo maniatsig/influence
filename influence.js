944019520f493eebf3260c793b65c423231f6966
maniatsig <tsigarida.mania@gmail.com>
2018-04-27 20:43:05 +0300

    Add files via upload

--- influence.js
+++ influence.js
@@ -239,5 +239,21 @@ function LoadData()
 function reset_def()
 {
 	window.localStorage.clear();
+	show_content("geoinfluence");
 
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
 }
